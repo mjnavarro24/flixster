a304028cc26c88f89c2d9925aa755ef2ad358af8
Kay Lab <kaylab@dhcp-10-8-058-098.mobile.reshsg.uci.edu>
2019-01-19 20:38:50 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Kay Lab on 1/19/19.
+//  Copyright © 2019 mjnavarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
